d6683de21022a9d078ef0d8a69fd9d1d3c71ecfb
balti salah <balti0salah@gmail.com>
2025-07-16 21:52:10 +0200

    WIP: local changes before pulling

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,25 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "23459df6-0f77-4545-8978-d77a59d0c8d7"
-
-
-}
-
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.0.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.25.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0"
+    }
   }
 }
+
+provider "azurerm" {
+  features {}
+  subscription_id = "23459df6-0f77-4545-8978-d77a59d0c8d7"
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
